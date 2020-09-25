002dab2c9b8d8e8b1af78f470d048bc84ebe0119
martin-garcia-blanco <programadopormartin@gmail.com>
2020-09-25 16:26:54 +0200

    :white_check_mark: Adding tests for aged brie with quality low than 50 and for 50

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -14,4 +14,20 @@ class GildedRoseTest {
         assertEquals("Frijolitos, 9, 9", app.items[0].toString());
     }
 
+    @Test
+    void should_increase_the_quality_twice_and_decrease_the_sellIn_for_an_aged_brie() {
+        Item[] items = new Item[] { new Item("Aged Brie", 9, 10) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals("Aged Brie, 8, 11", app.items[0].toString());
+    }
+
+    @Test
+    void when_the_quality_is_50_should_not_increase_the_quality_and_decrease_the_sellIn_for_an_aged_brie() {
+        Item[] items = new Item[] { new Item("Aged Brie", 9, 50) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals("Aged Brie, 8, 50", app.items[0].toString());
+    }
+
 }
