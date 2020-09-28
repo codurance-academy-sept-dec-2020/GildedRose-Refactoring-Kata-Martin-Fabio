acef20de96e84305a2442007d0cf7cd6bfbb735e
martin-garcia-blanco <programadopormartin@gmail.com>
2020-09-28 16:06:19 +0200

    :recycle: Extract strings into constants

--- scala/src/main/scala/com/gildedrose/GildedRose.scala
+++ scala/src/main/scala/com/gildedrose/GildedRose.scala
@@ -3,12 +3,16 @@ package com.gildedrose
 class GildedRose(val items: Array[Item]) {
 
 
+  private val AGE_BRIE = "Aged Brie"
+  private val BACKSTAGE_PASSES = "Backstage passes to a TAFKAL80ETC concert"
+  private val SULFURAS = "Sulfuras, Hand of Ragnaros"
+
   def updateQuality() {
     for (i <- 0 until items.length) {
-      if (!items(i).name.equals("Aged Brie")
-        && !items(i).name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+      if (!items(i).name.equals(AGE_BRIE)
+        && !items(i).name.equals(BACKSTAGE_PASSES)) {
         if (items(i).quality > 0) {
-          if (!items(i).name.equals("Sulfuras, Hand of Ragnaros")) {
+          if (!items(i).name.equals(SULFURAS)) {
             decreaseQuality(i)
           }
         }
@@ -16,7 +20,7 @@ class GildedRose(val items: Array[Item]) {
         if (items(i).quality < 50) {
           increaseQuality(i)
 
-          if (items(i).name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+          if (items(i).name.equals(BACKSTAGE_PASSES)) {
             if (items(i).sellIn < 11) {
               if (items(i).quality < 50) {
                 increaseQuality(i)
@@ -32,15 +36,15 @@ class GildedRose(val items: Array[Item]) {
         }
       }
 
-      if (!items(i).name.equals("Sulfuras, Hand of Ragnaros")) {
+      if (!items(i).name.equals(SULFURAS)) {
         items(i).sellIn = items(i).sellIn - 1
       }
 
       if (items(i).sellIn < 0) {
-        if (!items(i).name.equals("Aged Brie")) {
-          if (!items(i).name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+        if (!items(i).name.equals(AGE_BRIE)) {
+          if (!items(i).name.equals(BACKSTAGE_PASSES)) {
             if (items(i).quality > 0) {
-              if (!items(i).name.equals("Sulfuras, Hand of Ragnaros")) {
+              if (!items(i).name.equals(SULFURAS)) {
                 decreaseQuality(i)
               }
             }
