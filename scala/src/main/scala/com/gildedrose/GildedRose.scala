ce746836c2785205beace6f6872c26bc742ac083
nicolasbelfis <nicolas.belfis@codurance.com>
2020-09-28 16:42:58 +0200

    extract method and sorting the ifs

--- scala/src/main/scala/com/gildedrose/GildedRose.scala
+++ scala/src/main/scala/com/gildedrose/GildedRose.scala
@@ -11,10 +11,11 @@ class GildedRose(val items: Array[Item]) {
     for (i <- 0 until items.length) {
 
       if (items(i).name.equals(AGE_BRIE)) {
+        decreaseSellin(i)
         if (items(i).quality < 50)
           increaseQuality(i)
       } else if (items(i).name.equals(BACKSTAGE_PASSES)) {
-
+        decreaseSellin(i)
         if (items(i).quality < 50) {
           increaseQuality(i)
         }
@@ -30,48 +31,47 @@ class GildedRose(val items: Array[Item]) {
             increaseQuality(i)
           }
         }
+      } else if (items(i).name.equals(SULFURAS)) {
       } else {
+        decreaseSellin(i)
         if (items(i).quality > 0) {
-          if (!items(i).name.equals(SULFURAS)) {
-            decreaseQuality(i)
-          }
+          decreaseQuality(i)
         }
       }
 
-
-    if (!items(i).name.equals(SULFURAS)) {
-      items(i).sellIn = items(i).sellIn - 1
-    }
-
-    if (isSellinLessThan(i, 0)) {
-      if (!items(i).name.equals(AGE_BRIE)) {
-        if (!items(i).name.equals(BACKSTAGE_PASSES)) {
-          if (items(i).quality > 0) {
-            if (!items(i).name.equals(SULFURAS)) {
-              decreaseQuality(i)
+      if (isSellinLessThan(i, 0)) {
+        if (!items(i).name.equals(AGE_BRIE)) {
+          if (!items(i).name.equals(BACKSTAGE_PASSES)) {
+            if (items(i).quality > 0) {
+              if (!items(i).name.equals(SULFURAS)) {
+                decreaseQuality(i)
+              }
             }
+          } else {
+            items(i).quality = items(i).quality - items(i).quality
           }
         } else {
-          items(i).quality = items(i).quality - items(i).quality
-        }
-      } else {
-        if (items(i).quality < 50) {
-          increaseQuality(i)
+          if (items(i).quality < 50) {
+            increaseQuality(i)
+          }
         }
       }
     }
   }
-}
 
-private def isSellinLessThan (i: Int, sellIn: Int) = {
-  items (i).sellIn < sellIn
-}
+  private def decreaseSellin(i: Int) = {
+    items(i).sellIn = items(i).sellIn - 1
+  }
 
-  private def decreaseQuality (i: Int) = {
-  items (i).quality = items (i).quality - 1
-}
+  private def isSellinLessThan(i: Int, sellIn: Int) = {
+    items(i).sellIn < sellIn
+  }
 
-  private def increaseQuality (i: Int) = {
-  items (i).quality = items (i).quality + 1
-}
+  private def decreaseQuality(i: Int) = {
+    items(i).quality = items(i).quality - 1
+  }
+
+  private def increaseQuality(i: Int) = {
+    items(i).quality = items(i).quality + 1
+  }
 }
