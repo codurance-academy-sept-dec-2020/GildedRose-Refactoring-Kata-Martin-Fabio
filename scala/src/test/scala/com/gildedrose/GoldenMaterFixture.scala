c7611a360406128f8f0a081366aa622a6ac22511
martin-garcia-blanco <programadopormartin@gmail.com>
2020-09-28 15:33:02 +0200

    :sparkles: Check backstage passes and get 100% coverage

--- scala/src/test/scala/com/gildedrose/GoldenMaterFixture.scala
+++ scala/src/test/scala/com/gildedrose/GoldenMaterFixture.scala
@@ -7,7 +7,11 @@ object GoldenMaterFixture {
       new Item("Croisant",10,15),
       new Item("Croisant",10,0),
       new Item("Croisant",0,10),
-
+      new Item("Backstage passes to a TAFKAL80ETC concert", 15, 10),
+      new Item("Backstage passes to a TAFKAL80ETC concert", 8, 10),
+      new Item("Backstage passes to a TAFKAL80ETC concert", 2, 10),
+      new Item("Backstage passes to a TAFKAL80ETC concert", 0, 10),
+      new Item("Aged Brie",-6,10),
       new Item("Aged Brie",1,10))
     val gildedRose = new GildedRose(items)
 
