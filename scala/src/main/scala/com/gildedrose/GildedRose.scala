c6f586593c622772675c5554448feec66dbfb4c5
nicolasbelfis <nicolas.belfis@codurance.com>
2020-09-28 16:37:57 +0200

    reducing ifs

--- scala/src/main/scala/com/gildedrose/GildedRose.scala
+++ scala/src/main/scala/com/gildedrose/GildedRose.scala
@@ -10,30 +10,27 @@ class GildedRose(val items: Array[Item]) {
   def updateQuality() {
     for (i <- 0 until items.length) {
 
+      if (items(i).name.equals(AGE_BRIE)) {
+        if (items(i).quality < 50)
+          increaseQuality(i)
+      } else if (items(i).name.equals(BACKSTAGE_PASSES)) {
 
-      if (items(i).name.equals(AGE_BRIE)
-        || items(i).name.equals(BACKSTAGE_PASSES)) {
         if (items(i).quality < 50) {
           increaseQuality(i)
+        }
 
-          if (items(i).name.equals(BACKSTAGE_PASSES)) {
-            if (items(i).sellIn < 11) {
-              if (items(i).quality < 50) {
-                increaseQuality(i)
-              }
-            }
-
-            if (items(i).sellIn < 6) {
-              if (items(i).quality < 50) {
-                increaseQuality(i)
-              }
-            }
+        if (isSellinLessThan(i, 11)) {
+          if (items(i).quality < 50) {
+            increaseQuality(i)
           }
         }
-      }
 
-
-      else {
+        if (isSellinLessThan(i, 6)) {
+          if (items(i).quality < 50) {
+            increaseQuality(i)
+          }
+        }
+      } else {
         if (items(i).quality > 0) {
           if (!items(i).name.equals(SULFURAS)) {
             decreaseQuality(i)
@@ -41,35 +38,40 @@ class GildedRose(val items: Array[Item]) {
         }
       }
 
-      if (!items(i).name.equals(SULFURAS)) {
-        items(i).sellIn = items(i).sellIn - 1
-      }
 
-      if (items(i).sellIn < 0) {
-        if (!items(i).name.equals(AGE_BRIE)) {
-          if (!items(i).name.equals(BACKSTAGE_PASSES)) {
-            if (items(i).quality > 0) {
-              if (!items(i).name.equals(SULFURAS)) {
-                decreaseQuality(i)
-              }
+    if (!items(i).name.equals(SULFURAS)) {
+      items(i).sellIn = items(i).sellIn - 1
+    }
+
+    if (isSellinLessThan(i, 0)) {
+      if (!items(i).name.equals(AGE_BRIE)) {
+        if (!items(i).name.equals(BACKSTAGE_PASSES)) {
+          if (items(i).quality > 0) {
+            if (!items(i).name.equals(SULFURAS)) {
+              decreaseQuality(i)
             }
-          } else {
-            items(i).quality = items(i).quality - items(i).quality
           }
         } else {
-          if (items(i).quality < 50) {
-            increaseQuality(i)
-          }
+          items(i).quality = items(i).quality - items(i).quality
+        }
+      } else {
+        if (items(i).quality < 50) {
+          increaseQuality(i)
         }
       }
     }
   }
+}
 
-  private def decreaseQuality(i: Int) = {
-    items(i).quality = items(i).quality - 1
-  }
+private def isSellinLessThan (i: Int, sellIn: Int) = {
+  items (i).sellIn < sellIn
+}
 
-  private def increaseQuality(i: Int) = {
-    items(i).quality = items(i).quality + 1
-  }
+  private def decreaseQuality (i: Int) = {
+  items (i).quality = items (i).quality - 1
+}
+
+  private def increaseQuality (i: Int) = {
+  items (i).quality = items (i).quality + 1
+}
 }
