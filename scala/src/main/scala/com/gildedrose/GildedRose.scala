a6bfbe2ec4c3be31e8481ce7e19443c9f039e298
nicolasbelfis <nicolas.belfis@codurance.com>
2020-09-28 16:02:05 +0200

    refactoring : extract methods

--- scala/src/main/scala/com/gildedrose/GildedRose.scala
+++ scala/src/main/scala/com/gildedrose/GildedRose.scala
@@ -9,23 +9,23 @@ class GildedRose(val items: Array[Item]) {
         && !items(i).name.equals("Backstage passes to a TAFKAL80ETC concert")) {
         if (items(i).quality > 0) {
           if (!items(i).name.equals("Sulfuras, Hand of Ragnaros")) {
-            items(i).quality = items(i).quality - 1
+            decreaseQuality(i)
           }
         }
       } else {
         if (items(i).quality < 50) {
-          items(i).quality = items(i).quality + 1
+          increaseQuality(i)
 
           if (items(i).name.equals("Backstage passes to a TAFKAL80ETC concert")) {
             if (items(i).sellIn < 11) {
               if (items(i).quality < 50) {
-                items(i).quality = items(i).quality + 1
+                increaseQuality(i)
               }
             }
 
             if (items(i).sellIn < 6) {
               if (items(i).quality < 50) {
-                items(i).quality = items(i).quality + 1
+                increaseQuality(i)
               }
             }
           }
@@ -41,7 +41,7 @@ class GildedRose(val items: Array[Item]) {
           if (!items(i).name.equals("Backstage passes to a TAFKAL80ETC concert")) {
             if (items(i).quality > 0) {
               if (!items(i).name.equals("Sulfuras, Hand of Ragnaros")) {
-                items(i).quality = items(i).quality - 1
+                decreaseQuality(i)
               }
             }
           } else {
@@ -49,10 +49,18 @@ class GildedRose(val items: Array[Item]) {
           }
         } else {
           if (items(i).quality < 50) {
-            items(i).quality = items(i).quality + 1
+            increaseQuality(i)
           }
         }
       }
     }
+  }
+
+  private def decreaseQuality(i: Int) = {
+    items(i).quality = items(i).quality - 1
+  }
+
+  private def increaseQuality(i: Int) = {
+    items(i).quality = items(i).quality + 1
   }
 }
