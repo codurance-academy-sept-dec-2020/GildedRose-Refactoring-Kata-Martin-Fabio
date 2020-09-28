ff8250ef5b8217a293101596a02fe68d7bcacdfe
martin-garcia-blanco <programadopormartin@gmail.com>
2020-09-28 15:26:40 +0200

    :sparkles: Check normal items behaviour

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -1,6 +1,8 @@
 package com.gildedrose;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -8,10 +10,11 @@ class GildedRoseTest {
 
     @Test
     void should_decrease_the_quality_and_the_sellIn_once_for_normal_item() {
-        Item[] items = new Item[] { new Item("Frijolitos", 10, 10) };
+        Item item = new Item("Frijolitos", 10, 10);
+        Item[] items = new Item[] { item };
         GildedRose app = new GildedRose(items);
         app.updateQuality();
-        assertEquals("Frijolitos, 9, 9", app.items[0].toString());
+        assertEquals("Frijolitos, 9, 9", item.toString());
     }
 
     @Test
--- scala/src/test/scala/com/gildedrose/GoldenMaterFixture.scala
+++ scala/src/test/scala/com/gildedrose/GoldenMaterFixture.scala
@@ -3,11 +3,21 @@ package com.gildedrose
 object GoldenMaterFixture {
   def main(args: Array[String]): Unit = {
 
-    val items: Array[Item] = Array(new Item("Aged Brie",1,10))
-    val gildedRose = new GildedRose(items)
-    println(items(0).toString)
-    gildedRose.updateQuality()
-    println(items(0).toString)
+    val items: Array[Item] = Array(
+      new Item("Croisant",10,15),
+      new Item("Croisant",10,0),
+      new Item("Croisant",0,10),
 
+      new Item("Aged Brie",1,10))
+    val gildedRose = new GildedRose(items)
+
+    for (item <- items) {
+      println(item.toString)
+    }
+    println("\n -------------------")
+    gildedRose.updateQuality()
+    for (item <- items) {
+      println(item.toString)
+    }
   }
 }
